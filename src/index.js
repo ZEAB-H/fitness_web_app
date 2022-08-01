6c98fe1f115e6db7524c63151fa30291d289abda
abel-dejene <abelhailemichael30@gmail.com>
2022-08-01 04:12:22 +0300

    building the project on aug1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
@@ -9,5 +10,7 @@ const  root= ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render (
+    <BrowserRouter>
     <App/>
-)
+    </BrowserRouter>
+);
