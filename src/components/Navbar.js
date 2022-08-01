4911b41312b772f169ce61b6da52895b758426f6
abel-dejene <abelhailemichael30@gmail.com>
2022-08-01 05:03:01 +0300

    building the project on aug1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,20 @@ import { margin } from '@mui/system';
 
 const Navbar= () =>{
     return(
-        <Stack>
+        <Stack direction="row"
+            justifyContent="space-around"
+            sx={{gap:{sm:'122px', xs:'40px'} ,mt:{sm:'32px',xs:'20px'}}}>
             <Link to="/">
             <img src={Logo} alt="logo" style={{width:'48px', height:'48px', margin:'0 20px'}}/>
             </Link>
         
-        <Stack>
-            <Link to="/">Home</Link>
-            <a href="#exercises" style={{textDecoration: 'none', color:'#3A1212'}}></a>
+        <Stack
+        direction="row"
+        gap="40px"
+        fontsize="24px"
+        alignItems="flex-end">
+            <Link to="/" style={{textDecoration: 'none', color:'#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
+            <a href="#exercises" style={{textDecoration: 'none', color:'#3A1212'}}>Exercises</a>
         </Stack>
         </Stack>
     )
