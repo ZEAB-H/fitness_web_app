b33f4cc2fc8cafe95ba9849a32f63844ebbfcfa7
ZEAB-H <abelhailemichael30@gmail.com>
2022-09-09 13:22:52 +0300

    building homebanned component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import {Box, Stack, Typography} from '@mui/material';
 
 const HeroBanner= () =>{
     return(
-        <div>HeroBanner</div>
+        <Box sx={{
+            mt:{lg:'212px', xs:'70px'},
+            ml:{sm:'50px'}
+        }} position="relative" p="20px">
+            <Typography color="#FF2625" fontweight="600" fontsize="26px">
+                Fitness Club
+            </Typography>
+            <Typography fontweight={700} sx={{fontsize: {lg:'44px',xs:'40px'}}}>
+                Sweat, Smile <br/> and Repeat
+            </Typography>
+            <Typography fontsize="22px" lineHeight="35px">
+                Checkout the most effective exercises
+            </Typography>
+
+        </Box>
     )
 }
 
